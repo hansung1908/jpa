6986e7f81c6d0daf3591519655f6be5aa11bfb32
hansung1908 <hansung1908@naver.com>
2024-05-21 23:07:33 +0900

    환경 추가 설정 (실패)

--- src/main/java/com/hansung/Main.java
+++ src/main/java/com/hansung/Main.java
@@ -1,5 +1,24 @@
-package com.hansung;public class Main {
+package com.hansung;
+
+import com.hansung.entitiy.Customer;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("customer-exam");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        // 동작은 이 사이에서 진행
+        em.persist(Customer.sample());
+
+
+        tx.commit(); // tx.rollback();
+        em.close();
+        emf.close();
     }
 }
